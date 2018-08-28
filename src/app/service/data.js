9e393d063181ec4d8213150ef37ff0d01a454783
rui13th <rui13th@pulipinc.com>
2018-08-28 18:07:00 +0900

    cookie

--- src/app/service/data.js
+++ src/app/service/data.js
@@ -1,6 +1,12 @@
-const baseUrl = 'http://taskapi.pulipinc.com';
+// 로 실행시키고 호스트에
+
+// 127.0.0.1. localtask.pulipinc.com
+
+// 등로갛고 localtask.pulipinc.com로 접속하시면됩니다
+const baseUrl = 'http://taskapi.pulipinc.com'; //yarn start —disable-host-check —port 7000
 const header = {
     'POST' : {
+        'Accept': 'application/json',
         'Content-Type': 'application/json'
     }
 }
@@ -8,12 +14,20 @@ const methodInit = {
     'POST' : {
         method: 'POST',
         headers: header.POST,
-        body: JSON.stringify({"memberid":"schemak"})
+        credentials: 'include',
+        body: ''
     }    
 };
 export function getProjectListData(_params) {
     methodInit.POST.body = JSON.stringify(_params);   
     return fetch(baseUrl + '/getProjectList', methodInit.POST)
-        .then(_response =>  _response.json())
+        .then(_response => _response.json() )
+        .catch(err => console.log(err));
+}
+
+export function getProjectData(_params) {
+    methodInit.POST.body = JSON.stringify(_params);
+    return fetch(baseUrl + '/getData', methodInit.POST)
+        .then(_response => _response.json())
         .catch(err => console.log(err));
 }
